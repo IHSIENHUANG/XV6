27286218584f24e13e9010efd1c5684d2267bccb
I Hsien Huang <ihuan010@sledge.cs.ucr.edu>
2017-11-03 22:49:59 -0700

    test process

--- test.c
+++ test.c
@@ -6,15 +6,20 @@ int main(int argc, char *argv[])
 { 
     #if DEBUG
         int test = 0 ;
+        int another = 1;
+        if(another==1);
         for(test = 0; test < 10 ;test ++)
                hello();
     #endif
         int choice = atoi(argv[1]);
         if(choice ==1) info(1);
         else if(choice ==2)
-        {
+        {    
             info(2);
-            hello();
+            int times=3; 
+            int i = 0 ;
+            for( i =0;i<times ;i++)
+                hello();
             info(2);
         } 
         else if (choice ==3)
